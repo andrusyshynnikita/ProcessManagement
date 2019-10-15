4d4dff8624f249ff00d0f5081a5ceb85846a0987
Dev <Dev>
2019-10-15 11:25:13 +0300

    Add project files.

---
+++ ProcessManagement/ProcessManagement/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ProcessManagement
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
